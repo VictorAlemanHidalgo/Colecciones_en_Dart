76a66d182ab61a77aa0d48e8c76cd52807edb49e
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2025-02-12 20:55:33 +0000

    mapa1

---
+++ mapa1.dart
@@ -0,0 +1,20 @@
+void main() {
+  print("Victor Aleman Hidalgo 22308051281123 gpo 6 J");
+  Map<int, String> alumnos = {
+    1: "Juan", 
+    2: "Pedro", 
+    3: "Luis"
+    };
+  print("Mapa de alumnos:");
+  print(alumnos);
+
+  print("Iterar un mapa con forEach:");
+  alumnos.forEach((key, value) {
+    print("$key, $value");
+  });
+
+  print("iterar un mapa con for-in:");
+  for (var value in alumnos.values) {
+    print("$value");
+  }
+}
