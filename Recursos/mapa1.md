fa9d00adb75348f614b46b17deb0e2554d9b32ba
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2025-02-13 07:57:29 +0000

    Mapas ejemplo

---
+++ Recursos/mapa1.md
@@ -0,0 +1,4 @@
+Salida de datos de map
+![alt text](image-3.png)
+Otras salida usando ciclo for
+![alt text](image-4.png)
--- mapa1.dart
+++ mapa1.dart
@@ -1,20 +1,22 @@
-void main() {
+void main(){
   print("Victor Aleman Hidalgo 22308051281123 gpo 6 J");
   Map<int, String> alumnos = {
-    1: "Juan", 
-    2: "Pedro", 
-    3: "Luis"
-    };
-  print("Mapa de alumnos:");
+   1: "Juan",
+   2: "Pedro",
+   3: "Luis",};
+  print("Mapa de alumnos");
   print(alumnos);
 
-  print("Iterar un mapa con forEach:");
+  print("iterar un map con forEach");
   alumnos.forEach((key, value) {
-    print("$key, $value");
+    print("$key : $value");
+   
   });
 
-  print("iterar un mapa con for-in:");
+  print("iterar un map con for in");
   for (var value in alumnos.values) {
     print("$value");
   }
+
+
 }
